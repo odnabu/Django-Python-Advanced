5fb8558fa4dda5a623da923ea1be507478157527
odnabu <olga.v.dvornik@gmail.com>
2025-06-06 10:11:16 +0200

    Updated: .gitignore, DjangoProject_config/urls.py.

--- DjangoProject_config/urls.py
+++ DjangoProject_config/urls.py
@@ -18,7 +18,13 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    # path('les_01_begins/', include('les_01_begins.urls')),  # Подключение маршрутов приложения les_01_begins.
-    path('', include('hw_01_first_app.urls')),              # Подключение маршрутов приложения hw_01_first_app.
+    path('admin/', admin.site.urls),        # http://127.0.0.1:8000/admin/
+    path('les_01_begins/', include('les_01_begins.urls')),  # Подключение маршрутов приложения les_01_begins.
+                    # При этом в браузере нужно ввести: http://127.0.0.1:8000/les_01_begins/<адрес из VIEWS>.
+    # path('', include('hw_01_first_app.urls')),          # Подключение маршрутов ТОЛЬКО из приложения hw_01_first_app.
+                    # Если несколько приложений, то пустые кавычки '' здесь НЕЛЬЗЯ использовать!.
+    path('hw_01_first_app/', include('hw_01_first_app.urls')),  # Так нужно делать, если у меня в
+                    # проекте Пайчарм НЕ одно приложение, как сейчас: les_01_begins, hw_01_first_app.
+                    # При этом в браузере нужно ввести: http://127.0.0.1:8000/hw_01_first_app/<адрес из VIEWS>
+                    # (у меня для 1-ой функции ендпоинт hw-01/ и по нему выводится приветственное сообщение в браузере).
 ]
